2ddb2e26089bfea22096986fe2eb31922289ef6f
Patryk Karpinski <patryk.karpinski@igt.com>
2020-10-10 20:32:33 +0200

    Add gh-pages deploy config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { title, keywords, description, author, defaultLang, trackingId } = require("./config/site");
 
 module.exports = {
+  pathPrefix: "/reponame",
   siteMetadata: {
     title,
     keywords,
